21e5811058d2dc41722b4641bb6845ce2b73472e
emmettmccann <mccannemmett@gmail.com>
2020-04-20 13:11:20 -0700

    genetic tree parsing

--- NextStrain Data/nextstrainParser.js
+++ NextStrain Data/nextstrainParser.js
@@ -1,51 +1,8 @@
-var schema = {
-  type: "genome",
-  id: "Shanghai/SH0020/2020",
-  sampled: true,
-  author: "Wang et al",
-  country: "China",
-  division: "Shanghai",
-  location: "Snohomish County",
-  gisaid_epi_isl: "EPI_ISL_416329",
-  date: "2020.0860655737704",
-  originating_lab: "Shanghai Public Health Clinical Center, Shanghai Medical College, Fudan University",
-  region: "Asia",
-  sex: "Female",
-  submitting_lab:
-    "National Research Center for Translational Medicine (Shanghai), Ruijin Hospital affiliated to Shanghai Jiao Tong University School of Medicine & Shanghai Public Health Clinical Center",
-};
-
-var nodeSchema = {
-  type: "genome",
-  id: "NODE_0000720",
-  sampled: false,
-  date: "2020.0722122686764",
-  date_confidence_low: 2020.0461236342921,
-  date_confidence_high: 2020.084336441549,
-  country: "China",
-  country_confidence: "0.999964205852087",
-};
-
-var mutationSchema = {
-  type: "mutated",
-  id: "__sourcename__-__destname__",
-  aa: "__pull from labels__",
-  nuc: "__comma separated__",
-};
-
-var geneNode = {
-  type: "gene",
-  id: "ORF1a",
-  range: "266..13468",
-};
-
 const fs = require("fs");
 const path = require("path");
 
 let rawdata = fs.readFileSync(path.resolve(__dirname, "./global-4-17-genetic-tree.json"));
 let africa = JSON.parse(rawdata);
-
-// console.log(africa.tree);
 
 let nodes = [];
 let links = [];
@@ -71,48 +28,6 @@ function extractNode(parentName, node) {
   dates.push(d);
   if (node.children) node.children.forEach((el) => extractNode(node.name, el));
 }
-
-function getMutation(link) {
-  let res = link;
-  res.id = link.parent + "-" + link.child;
-  res.aa = link.mutation && link.mutation.labels ? link.mutation.labels.aa : undefined;
-  res.nuc =
-    link.mutation && link.mutation.mutations && link.mutation.mutations.nuc
-      ? link.mutation.mutations.nuc
-          .reduce((prev, curr) => {
-            return prev + "," + curr;
-          }, "")
-          .slice(1)
-      : undefined;
-
-  Object.keys(res).forEach((key) => res[key] === undefined && delete res[key]);
-  return res;
-}
-
-function getAttrs(node_attrs) {
-  let res = {};
-  res.author = getDef(node_attrs.author);
-  res.date = getDef(node_attrs.num_date);
-  res.region = getDef(node_attrs.region);
-  res.country = getDef(node_attrs.country);
-  res.location = getDef(node_attrs.location);
-  res.sex = getDef(node_attrs.sex);
-  res.division = getDef(node_attrs.division);
-  res.lab = getDef(node_attrs.originating_lab);
-
-  if (node.name.slice(0, 5) == "NODE_") {
-    res.status = "inferred";
-    res.date_confidence_low = node.node_attrs.num_date.confidence[0];
-    res.date_confidence_high = node.node_attrs.num_date.confidence[1];
-  } else {
-    res.status = "sampled";
-  }
-
-  Object.keys(res).forEach((key) => res[key] === undefined && delete res[key]);
-  return res;
-}
-
-extractNode("root", africa.tree);
 
 function formatGenomeNode(node) {
   return {
@@ -145,7 +60,7 @@ function formatGenomeNode(node) {
 
 function formatLink(link) {
   return {
-    type: "link",
+    type: "mutated",
     id: link.parent + "-" + link.child,
     parent: link.parent,
     child: link.child,
@@ -161,8 +76,35 @@ function formatLink(link) {
   };
 }
 
+function formatDateLink(date) {
+  return {
+    type: "at",
+    id: date.parent + "@" + date.attrs.num_date.value,
+    parent: date.parent,
+    child: (() => {
+      let d = getDef(date.attrs.num_date);
+      if (d == undefined) return undefined;
+      d = new Date((d - 1970) * 365.25 * 24 * 60 * 60 * 1000); // convert the datetime to proper date object
+      return d.toISOString().substring(0, 10);
+    })(),
+  };
+}
+
+// recursively extract all info from the tree
+extractNode("root", africa.tree);
+
+// format each genome node and mutation relationships
 nodes = nodes.map(formatGenomeNode);
 links = links.map(formatLink);
+// format date links for mapping to date nodes
+dates = dates.map(formatDateLink);
+
+console.log("Generated %d genome nodes of format: ", nodes.length);
+console.log(nodes[1]);
+console.log("Generated %d mutation links of format: ", links.length);
+console.log(links[1]);
+console.log("Generated %d date links of format: ", dates.length);
+console.log(dates[1]);
 
 fs.writeFileSync("nodes.json", JSON.stringify(nodes));
 fs.writeFileSync("links.json", JSON.stringify(links));

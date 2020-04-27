0e225f3e0d0b9243ba4617cb05fc60d1b2e6b7da
emmettmccann <mccannemmett@gmail.com>
2020-04-27 03:42:50 -0700

    more pk toString bugs

--- data/cosmos/client.js
+++ data/cosmos/client.js
@@ -55,7 +55,7 @@ client.addVertex = function (v, pk) {
   Object.keys(v).forEach((k) => {
     query += ".property('" + k + "', " + k + ")";
   });
-  query += ".property('pk', '" + (toString(pk) || "testpk") + "')";
+  query += ".property('pk', '" + (pk.toString() || "testpk") + "')";
   return client.submit(query, v);
 };
 

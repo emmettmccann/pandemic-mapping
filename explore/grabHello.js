5b11e47d350f989c2e967efbf8cc97ccc2474559
emmettmccann <mccannemmett@gmail.com>
2020-04-27 02:37:06 -0700

    save requirements

--- explore/grabHello.js
+++ explore/grabHello.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 let nodes = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./testobj.json")));
 
 console.log(JSON.stringify(nodes));

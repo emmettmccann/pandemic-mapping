7919653008fe8b8f2489ba7e449871c61e95752e
emmettmccann <mccannemmett@gmail.com>
2020-04-27 01:48:23 -0700

    genome seed only includes tree nodes and edges

--- data/cosmos/seedGenetics.js
+++ data/cosmos/seedGenetics.js
@@ -1,19 +1,16 @@
 const g = require("./client");
-const fs = require("fs");
-const path = require("path");
 const currDate = require("../static/currDate");
 
 // get basic stats on the current db
-g.open().then(g.count).then(g.close);
+// g.open().then(g.count).then(g.close);
 
 // upload from today's nextstrain files
-// g.open()
-//   .then(dropGeneticTree)
-//   .then(() => g.addNodesFromFile("../artifacts/nodes-" + currDate + ".json"))
-//   .then(() => g.addLinksFromFile("../artifacts/links-" + currDate + ".json"))
-//   .then(() => g.addLinksFromFile("../artifacts/dates-" + currDate + ".json"))
-//   .then(g.count)
-//   .then(finish);
+g.open()
+  .then(dropGeneticTree)
+  .then(() => g.addNodesFromFile("../artifacts/nodes-" + currDate + ".json"))
+  .then(() => g.addLinksFromFile("../artifacts/links-" + currDate + ".json"))
+  .then(g.count)
+  .then(g.finish);
 
 function dropGeneticTree() {
   return g.submit("g.V().hasLabel('genome').drop()", {});

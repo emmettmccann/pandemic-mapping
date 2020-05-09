fc1bc74b2b4f85e1e88985e8c9de69dd7e9789d4
emmettmccann <mccannemmett@gmail.com>
2020-05-09 13:31:50 -0700

    add states and probables route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,43 @@ var db = require("../db/neo.js");
 router.get("/", function (req, res, next) {
   let session = db.getSession(req);
   session.run("MATCH (n) RETURN n limit 1").then((response) => {
-    res.send(response);
+    db.closeSession(req);
+    const singleRecord = response.records[0];
+    const node = singleRecord.get(0);
+    res.json(node);
   });
-  // res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get("/states", function (req, res, next) {
+  let session = db.getSession(req);
+  session.run("MATCH (state:state) RETURN state").then((result) => {
+    db.closeSession(req);
+    const response = result.records.map((record) => {
+      return record.get("state");
+    });
+    res.json(response);
+  });
+});
+
+router.get("/probables", function (req, res, next) {
+  let prob = parseFloat(req.query.prob);
+  if (isNaN(prob)) prob = 0.4; // do it this way to allow prob to be 0
+
+  let session = db.getSession(req);
+  const q =
+    "WITH ['WA','NY'] as states MATCH (loc:state)<-[samp:SAMPLED_IN]-(n:genome)-[r:PROBABLE_SOURCE]->(parent:state) where r.avg > $prob and loc.id <> parent.id and loc.id in states with loc as loc, parent as parent, n.date_formatted as date, r.avg as prob CALL apoc.create.vRelationship(parent,'SEEDED',{date:date,prob:prob},loc) yield rel as seed return loc,parent,seed order by loc.id";
+  session.run(q, { prob: prob }).then((result) => {
+    db.closeSession(req);
+    const response = result.records.map((record) => {
+      return {
+        source: record.get("parent"),
+        location: record.get("loc"),
+        transmission: record.get("seed"),
+      };
+    });
+    res.json(response);
+  });
 });
 
 module.exports = router;

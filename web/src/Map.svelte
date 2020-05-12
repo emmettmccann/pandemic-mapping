dc564b7faf45f683072ec3a5ab9824e3cc2084d0
emmettmccann <mccannemmett@gmail.com>
2020-05-12 12:00:02 -0700

    add map

---
+++ web/src/Map.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { onMount, setContext } from "svelte";
+  import { mapbox, key } from "./mapbox.js";
+
+  setContext(key, {
+    getMap: () => map
+  });
+
+  export let lat;
+  export let lon;
+  export let zoom;
+
+  let container;
+  let map;
+
+  $: console.log(map);
+
+  onMount(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://unpkg.com/mapbox-gl/dist/mapbox-gl.css";
+
+    link.onload = () => {
+      map = new mapbox.Map({
+        container,
+        style: "mapbox://styles/emmettmccann/ck8z41m4f014n1inw6emiz6hk",
+        center: [lon, lat],
+        zoom,
+        attributionControl: false
+      });
+      // map.getCanvas().style.cursor = "crosshair";
+    };
+
+    document.head.appendChild(link);
+
+    return () => {
+      map.remove();
+      link.parentNode.removeChild(link);
+    };
+  });
+</script>
+
+<style>
+  div {
+    position: absolute;
+    top: 0px;
+    left: 25vw;
+    height: 100vh;
+    width: 75vw;
+    /* cursor: crosshair; */
+  }
+</style>
+
+<div bind:this={container}>
+  {#if map}
+    <slot />
+  {/if}
+</div>
---
+++ web/src/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from "mapbox-gl";
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = "pk.eyJ1IjoiZW1tZXR0bWNjYW5uIiwiYSI6ImNrOHl1bW42aTFtMnYzdWpzbXl6c2hhOTAifQ.vZOy44ZEfFIjx2fuW3-C0Q";
+
+const key = {};
+
+export { mapbox, key };
